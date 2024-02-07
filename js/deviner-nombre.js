300451a951c0daf50461705ce3e1ff623df7df03
Loris Gschwind <124797573+lorisgschwind@users.noreply.github.com>
2024-02-07 09:53:24 +0100

    init js

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,14 +1,20 @@
 /**
  * Jeu du nombre mystère
  * @author  Steve Fallet
- * @version 1.0
- * @since   2022-08-30 (date de création)
+ * @version 2.0
+ * @since   2018-09-09 (date de création)
  */
 
-//Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
-(function main() {
-    'use strict';
+'use strict';
 
-   // Ton code de malade ici ...
+/**
+ * Retourne un nombre entier aléatoire compris entre min et max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function tireNombre(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
-}()); //main IIFE
+alert(tireNombre(50,150));
